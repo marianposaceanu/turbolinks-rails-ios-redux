d5b65df29fb8b8722d2072783c362f26184d6cd5
Marian Posaceanu <contact@marianposaceanu.com>
2016-07-20 00:07:10 +0300

    Use custom agent and follow links in the app

--- ios-turbolinks-5-app/ios-turbolinks-5-app/AppDelegate.swift
+++ ios-turbolinks-5-app/ios-turbolinks-5-app/AppDelegate.swift
@@ -7,13 +7,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     var navigationController = UINavigationController()
-    var session = Session()
-    
-    // MARK: UIApplicationDelegate
+    lazy var session: Session = {
+        let configuration = WKWebViewConfiguration()
+        configuration.applicationNameForUserAgent = "Turbolinks 5 Demo"
+        return Session(webViewConfiguration: configuration)
+    }()
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         window?.rootViewController = navigationController
+        session.delegate = self
         
         startApplication()
         
@@ -30,3 +33,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         session.visit(visitableViewController)
     }
   }
+
+extension AppDelegate: SessionDelegate {
+    func session(session: Session, didProposeVisitToURL URL: NSURL, withAction action: Action) {
+        visit(URL)
+    }
+    
+    func session(session: Session, didFailRequestForVisitable visitable: Visitable, withError error: NSError) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        navigationController.presentViewController(alert, animated: true, completion: nil)
+    }
+}
